c04af7901676822ae5f382694788921fe27e8f53
Oliwia <itsechi@gmail.com>
2024-04-22 16:50:43 +0200

    Change the design of the dashboard, add a form to have lists

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,43 +1,44 @@
-import { TrashIcon } from '@heroicons/react/24/outline';
+import { PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
 const Dashboard = () => {
-  const tasks = [];
+  const [showInput, setShowInput] = useState(false);
 
   return (
     <main className="h-[calc(100vh-60px)] p-4 text-black dark:bg-night-bg dark:text-white">
-      <div className="mx-auto max-w-screen-md">
+      <div className="mx-auto max-w-screen-2xl">
         <h1 className="mt-4 text-3xl font-bold">Dashboard</h1>
-        <form className="mt-4 flex w-full gap-2">
-          <input
-            className="text-md flex-grow rounded-lg border bg-hover p-2 focus:outline-primary dark:border-night-border dark:bg-night-hover dark:focus:outline-none dark:focus:outline-primary"
-            type="text"
-            placeholder="Enter your task here"
-          />
-          <button className="rounded-lg bg-primary px-4 py-2 font-semibold text-white hover:bg-hover-primary dark:bg-primary">
-            Add Task
-          </button>
-        </form>
-
-        {tasks.length > 0 ? (
-          <div className="mt-4 ">
-            {tasks.map(task => (
-              <div className="group mb-2 flex w-full items-center rounded-lg border px-4 py-3 dark:border-night-border">
-                <input className="mr-2" type="checkbox" />
-                <p>{task.task}</p>
-                <button className="ml-auto opacity-0 group-hover:opacity-100">
-                  <TrashIcon className="h-6 w-6" />
-                </button>
-              </div>
-            ))}
-          </div>
+        {showInput ? (
+          <form className="mt-4 flex max-w-[300px] flex-col gap-2 rounded-md border p-4 dark:border-night-border">
+            <input
+              className="rounded-lg border bg-hover px-2 py-1.5 text-sm focus:outline-primary dark:border-night-border dark:bg-night-hover dark:focus:outline-none dark:focus:outline-primary"
+              type="text"
+              placeholder="Enter the list name"
+            />
+            <div className="flex gap-2 text-sm">
+              <button
+                className="rounded-lg bg-primary px-4 py-2 font-semibold text-white hover:bg-hover-primary dark:bg-primary
+              "
+              >
+                Add List
+              </button>
+              <button
+                type="button"
+                className="rounded-md border  bg-white px-4 py-2 font-semibold  shadow-sm  hover:bg-hover dark:border-night-border dark:bg-night-bg dark:text-night-gray-text dark:hover:bg-night-nav"
+                onClick={() => setShowInput(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
         ) : (
-          <div className="mt-4 flex flex-grow flex-col items-center justify-center rounded-lg border py-14 text-center dark:border-night-border">
-            <h3 className="text-lg font-bold">Add tasks</h3>
-            <p className="text-gray-text">
-              You don't have any tasks yet. Enter your first task in the field
-              above!
-            </p>
-          </div>
+          <button
+            onClick={() => setShowInput(!showInput)}
+            className="mt-4 flex w-[300px] items-center gap-2 rounded-md border  bg-white px-4 py-2 text-sm  font-semibold  shadow-sm hover:bg-hover dark:border-night-border dark:bg-night-bg dark:text-night-gray-text dark:hover:bg-night-nav"
+          >
+            <PlusIcon className="h-4 w-4" />
+            Add your first list
+          </button>
         )}
       </div>
     </main>
