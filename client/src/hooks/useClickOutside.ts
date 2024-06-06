ca57fd6003ca87df590a4fb096c1730c8b0513ab
Oliwia <itsechi@gmail.com>
2024-06-06 18:54:45 +0200

    Refactor the useClickOutside function (fix nexted conditions and return early)

--- client/src/hooks/useClickOutside.ts
+++ client/src/hooks/useClickOutside.ts
@@ -1,27 +1,34 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 
 export function useClickOutside(callback?: () => void, input?: string) {
   const [isVisible, setIsVisible] = useState(false);
   const ref = useRef<HTMLDivElement | HTMLFormElement>(null);
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        if (!callback) return setIsVisible(false);
-        else {
-          if (input !== undefined && input.trim().length === 0) {
-            return;
-          } else {
-            callback();
-            setIsVisible(false);
-          }
-        }
+
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
       }
-    }
+
+      if (!callback) {
+        setIsVisible(false);
+        return;
+      }
+
+      if (input === undefined || input.trim().length > 0) {
+        callback();
+        setIsVisible(false);
+      }
+    },
+    [callback, input],
+  );
+
+  useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [ref, callback, input]);
+  }, [handleClickOutside]);
 
   return { ref, isVisible, setIsVisible };
 }
