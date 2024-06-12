e31462fed60f344221c9fea4d69c0a3a129beebf
Oliwia <itsechi@gmail.com>
2024-06-12 18:01:13 +0200

    Install gh-pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/typescript-todo/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
